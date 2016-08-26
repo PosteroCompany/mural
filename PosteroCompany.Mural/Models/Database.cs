76dd3d54112a45c29279f3e9d931a74c09404e68
felipemfp <felipemfpontes@gmail.com>
2016-08-26 12:32:06 -0300

    Add Configuration Manager

--- PosteroCompany.Mural/Models/Database.cs
+++ PosteroCompany.Mural/Models/Database.cs
@@ -13,7 +13,7 @@ namespace PosteroCompany.Mural.Models
         // If you wish to target a different database and/or database provider, modify the 'Database' 
         // connection string in the application configuration file.
         public Database()
-            : base("name=Database")
+            : base(ConfigurationManager.Get("MURAL_CONNECTION_STRING") as string)
         {
         }
 
