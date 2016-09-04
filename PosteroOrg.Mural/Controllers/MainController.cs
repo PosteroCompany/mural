40c5d6d925b4460b574fd02d8163a42fc1c539b4
felipemfp <felipemfpontes@gmail.com>
2016-09-04 14:47:04 -0300

    Add alerts

--- PosteroOrg.Mural/Controllers/MainController.cs
+++ PosteroOrg.Mural/Controllers/MainController.cs
@@ -39,7 +39,16 @@ namespace PosteroOrg.Mural.Controllers
                 if (user != null && BCryptHelper.CheckPassword(password, user.Password))
                 {
                     Session["User"] = user;
+                    Alert("Você foi autenticado com sucesso.", type: "success");
                 }
+                else
+                {
+                    Alert("Note que ambos os campos diferenciam letras maiúsculas e minúsculas.", "Não reconhecemos suas credenciais.", "danger");
+                }
+            }
+            else
+            {
+                Alert("Parece que você esqueceu de informar seu usuário ou sua senha.", "Ops.", "danger");
             }
             return RedirectToAction("");
         }
@@ -48,20 +57,44 @@ namespace PosteroOrg.Mural.Controllers
         [HttpPost]
         public ActionResult Register(string username, string password, string passwordCheck)
         {
-            if (!String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password) && password == passwordCheck && db.Users.Find(username) == null)
+            if (!String.IsNullOrWhiteSpace(username) && !String.IsNullOrWhiteSpace(password))
             {
-                if (Regex.IsMatch(username, @"^[a-zA-Z0-9_]{6,}$") && Regex.IsMatch(password, @"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"))
+                if (password == passwordCheck)
                 {
-                    User user = new User()
+                    if (Regex.IsMatch(username, @"^[a-zA-Z0-9_]{6,}$") && Regex.IsMatch(password, @"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"))
                     {
-                        Username = username.Trim(),
-                        Password = BCryptHelper.HashPassword(password, BCryptHelper.GenerateSalt()),
-                        DtRegister = DateTimeOffset.Now
-                    };
-                    db.Users.Add(user);
-                    db.SaveChanges();
-                    Session["User"] = user;
-                }                
+                        if (db.Users.Find(username) == null)
+                        {
+                            User user = new User()
+                            {
+                                Username = username.Trim(),
+                                Password = BCryptHelper.HashPassword(password, BCryptHelper.GenerateSalt()),
+                                DtRegister = DateTimeOffset.Now
+                            };
+                            db.Users.Add(user);
+                            db.SaveChanges();
+                            Session["User"] = user;
+                            Alert("Você foi cadastrado com sucesso.", "Bem vindo.", "success");
+                        }
+                        else
+                        {
+                            Alert("Parece que alguém já escolheu esse nome de usuário.", "Usuário já existe.", "danger");
+                        }
+                    }
+                    else
+                    {
+                        Alert("Note que sua senha deve ter pelo menos um número, uma letra maiúscula, uma letra minúscula e 8 caractéres ou mais. E escolha seu nome de usuário utilizando apenas caractéres alfanuméricos e traço baixo, e deve ter pelo menos 6 caractéres.", "Informações inválidas.", "danger");
+                    }
+                }
+                else
+                {
+                    Alert("Parece que você sua senha não confere com a confirmação.", "Ops.", "danger");
+                }
+
+            }
+            else
+            {
+                Alert("Parece que você esqueceu de informar um nome de usuário ou uma senha.", "Ops.", "danger");
             }
             return RedirectToAction("");
         }
@@ -70,6 +103,7 @@ namespace PosteroOrg.Mural.Controllers
         public ActionResult Logout()
         {
             Session["User"] = null;
+            Alert("Você foi desconectado com sucesso.", "Desconectado.");
             return RedirectToAction("");
         }
 
@@ -81,13 +115,31 @@ namespace PosteroOrg.Mural.Controllers
             if (Session["User"] != null && !String.IsNullOrWhiteSpace(note))
             {
                 User user = db.Users.Find((Session["User"] as User).Username);
-                user.Notes.Add(new Note() {
+                user.Notes.Add(new Note()
+                {
                     PureContent = note,
                     DtNote = DateTimeOffset.Now
                 });
                 db.SaveChanges();
+                Alert("Sua nova nota foi registrada com sucesso.", "Salvo.", "success");
+            }
+            else
+            {
+                Alert("Parece que você esqueceu de escrever a nota.", "Ops.", "danger");
             }
             return RedirectToAction("");
+        }
+
+        protected void Alert(string message, string title = "", string type = "")
+        {
+            var alerts = TempData["Alerts"] != null ? (List<Models.Alert>)TempData["Alerts"] : new List<Models.Alert>();
+            alerts.Add(new Models.Alert
+            {
+                Message = message,
+                Title = title,
+                Type = type
+            });
+            TempData["Alerts"] = alerts;
         }
 
         protected override void Dispose(bool disposing)
